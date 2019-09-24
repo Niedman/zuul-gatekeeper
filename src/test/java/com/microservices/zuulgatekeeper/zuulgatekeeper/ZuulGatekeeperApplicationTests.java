3015bc8c4c4c7e8dc137a337af35029954953ba2
Nino Matos <niedman@gmail.com>
2019-09-24 13:54:22 +0200

    Initial commit

---
+++ src/test/java/com/microservices/zuulgatekeeper/zuulgatekeeper/ZuulGatekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.zuulgatekeeper.zuulgatekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
